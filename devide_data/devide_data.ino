a00f0cfbe53c8c71bd22242b5d0c98e8ff4c134c
M Hendri F <hendrifebriansyah28@gmail.com>
2018-05-13 16:16:59 +0700

    update

--- devide_data/devide_data.ino
+++ devide_data/devide_data.ino
@@ -5,7 +5,10 @@
 
 #define arduinoLED 13   // Arduino LED on board
 
-void removeChar( char * string, char letter );
+#define BUFFER_SIZE 700
+uint8_t orig_buffer[BUFFER_SIZE];
+uint8_t comp_buffer[BUFFER_SIZE];
+uint8_t decomp_buffer[BUFFER_SIZE];
 
 int main(int argc, char **argv)
 {
@@ -14,26 +17,42 @@ int main(int argc, char **argv)
     digitalWrite(arduinoLED, LOW);
     Serial.begin(9600);
     Serial.println("Incoming data :");
-    int num[200];
-    char incomingByte;
-    int i=0, length_data, sisa;
+    int i=0, length_data, sisa, nlevel;
     int j=0;
-    int n=0;
+    int n=0, idx;
     String stringOne;
     int orig=0, window=0, lookahead=0, decoder=0, polled=0;
     char *s, *orig_sz, *window_sz, *lookahead_sz, *decoder_sz, *polled_sz;
-    String stringTwo = "a100a";
-    char buf[20];
+
+    uint8_t test_data [] = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+    length_data = sizeof(test_data)/sizeof(test_data[0]);
+    Serial.println(length_data);
     
-    size_t comp_sz = 200;
-    memset(num,0,comp_sz);
-  
     double ndata;
-    ndata = (double)1200 / 584;
+    ndata = (double)length_data / 584;
+    nlevel = (int)ceil(ndata);
+    sisa = length_data % 548;
+    
     Serial.print("N data : ");
-    Serial.println((int)ceil(ndata));
+    Serial.println(nlevel);
     Serial.print("sisa : ");
-    Serial.println(1000 % 548);
+    Serial.println(sisa);
+
+//    for(i=0; i<nlevel; i++)
+//    {
+//      uint32_t comp_size   = BUFFER_SIZE; //this will get updated by reference
+//      uint32_t decomp_size = BUFFER_SIZE; //this will get updated by reference
+//      //memcpy(orig_buffer, test_data, length_data);
+//      orig_buffer[0] = "c";
+//      Serial.print(orig_buffer[0]);
+//      memset(orig_buffer,10,BUFFER_SIZE);
+//      for(j=0; j<40; j++){
+//        Serial.print(orig_buffer[j]);
+//      }
+//      //memset
+//      //copy data dari index x sampai dindex n
+//      delay(6000);
+//    }
 
     int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
     length_data = sizeof(array)/sizeof(int);
@@ -42,27 +61,29 @@ int main(int argc, char **argv)
       /* handle error */
       Serial.println(F("FAIL: Malloc fail!"));
     }
-    memcpy(firstHalf, array, 10 * sizeof(int));
+//    memcpy( destination + N, source + N, sourceLen - N );
+//    memcpy(firstHalf, array, 10 * sizeof(int));
+    memcpy(firstHalf, array + 2, 10 * sizeof(int));
     sisa = length_data - 10;
 
-//    for(int i=0; i<10; i++){
-//       Serial.println(firstHalf[i]);
-//    }
-        
-    if(length_data > 10){
-        int *secondHalf = malloc(sisa* sizeof(int));
-        if (!secondHalf) {
-          /* handle error */
-          Serial.println(F("FAIL: Malloc fail!"));
-        }
-
-        memcpy(secondHalf, array + 10, sisa * sizeof(int));
-
-         
-        for(int i=0; i<sisa; i++){
-           Serial.println(secondHalf[i]);
-        }
+    for(int i=0; i<10; i++){
+       Serial.print(firstHalf[i]);
     }
+        
+//    if(length_data > 10){
+//        int *secondHalf = malloc(sisa* sizeof(int));
+//        if (!secondHalf) {
+//          /* handle error */
+//          Serial.println(F("FAIL: Malloc fail!"));
+//        }
+//
+//        memcpy(secondHalf, array + 10, sisa * sizeof(int));
+//
+//         
+//        for(int i=0; i<sisa; i++){
+//           Serial.println(secondHalf[i]);
+//        }
+//    }
    
     //Serial.println(length_data);
     //Serial.println(sizeof(firstHalf));
@@ -71,6 +92,6 @@ int main(int argc, char **argv)
     for ( ;; )
     {
         Serial.println("test");
-        delay(5000);
+        delay(9000);
     }
 }
