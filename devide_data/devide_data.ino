f5ce39ec05012393ae34aecf30dedefad78bd305
M Hendri F <hendrifebriansyah28@gmail.com>
2018-05-13 16:58:50 +0700

    udah bisa bagi beradasarkan panjangnya

--- devide_data/devide_data.ino
+++ devide_data/devide_data.ino
@@ -24,70 +24,99 @@ int main(int argc, char **argv)
     int orig=0, window=0, lookahead=0, decoder=0, polled=0;
     char *s, *orig_sz, *window_sz, *lookahead_sz, *decoder_sz, *polled_sz;
 
-    uint8_t test_data [] = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
-    length_data = sizeof(test_data)/sizeof(test_data[0]);
-    Serial.println(length_data);
-    
-    double ndata;
-    ndata = (double)length_data / 584;
-    nlevel = (int)ceil(ndata);
-    sisa = length_data % 548;
-    
-    Serial.print("N data : ");
-    Serial.println(nlevel);
-    Serial.print("sisa : ");
-    Serial.println(sisa);
-
+    uint8_t test_data [] = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+//    length_data = sizeof(test_data)/sizeof(test_data[0]);
+//    Serial.println(length_data);
+//    
+//    double ndata;
+//    ndata = (double)length_data / 584;
+//    nlevel = (int)ceil(ndata);
+//    sisa = length_data % 548;
+//    
+//    Serial.print("N data : ");
+//    Serial.println(nlevel);
+//    Serial.print("sisa : ");
+//    Serial.println(sisa);
+//
+//    uint32_t comp_size   = BUFFER_SIZE; //this will get updated by reference
+//    uint32_t decomp_size = BUFFER_SIZE; //this will get updated by reference
+//      
 //    for(i=0; i<nlevel; i++)
 //    {
-//      uint32_t comp_size   = BUFFER_SIZE; //this will get updated by reference
-//      uint32_t decomp_size = BUFFER_SIZE; //this will get updated by reference
+//      Serial.println("Memset comp_buffer");
+//      //copy 584 char to comp_buffer
+//      if(nlevel>1){
+//        //set destination index + 584
+//      }
+//      //set index awal += 584
 //      //memcpy(orig_buffer, test_data, length_data);
 //      orig_buffer[0] = "c";
 //      Serial.print(orig_buffer[0]);
 //      memset(orig_buffer,10,BUFFER_SIZE);
-//      for(j=0; j<40; j++){
-//        Serial.print(orig_buffer[j]);
-//      }
-//      //memset
 //      //copy data dari index x sampai dindex n
-//      delay(6000);
+//      delay(3000);
 //    }
+    ////////////////////////////////////////////////////////////////////////////
+    int test_buffer[100];
+   
+    int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    length_data = sizeof(array)/sizeof(array[0]);
+    Serial.println(length_data);
 
-    int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    length_data = sizeof(array)/sizeof(int);
-    int *firstHalf = malloc(10 * sizeof(int));
-    if (!firstHalf) {
-      /* handle error */
-      Serial.println(F("FAIL: Malloc fail!"));
-    }
-//    memcpy( destination + N, source + N, sourceLen - N );
-//    memcpy(firstHalf, array, 10 * sizeof(int));
-    memcpy(firstHalf, array + 2, 10 * sizeof(int));
-    sisa = length_data - 10;
+    double ndata;
+    ndata = (double)length_data / 6;
+    nlevel = (int)ceil(ndata);
+    sisa = length_data % 6;
 
-    for(int i=0; i<10; i++){
-       Serial.print(firstHalf[i]);
+    Serial.print("N data : ");
+    Serial.println(nlevel);
+    Serial.print("sisa : ");
+    Serial.println(sisa);
+    idx = 0;
+    int idx_normal = 6;
+    for(i=0; i<nlevel; i++)
+    {
+      Serial.println("Memset comp_buffer");
+      memset(test_buffer,0,100);
+      //copy 584 char to comp_buffer
+      if(i<nlevel-1){
+        memcpy(test_buffer, array+idx, idx_normal * sizeof(int));
+      }
+      else{
+        memcpy(test_buffer, array+idx, sisa * sizeof(int));
+      }
+      idx = idx + 6;
+      Serial.print("idx now : ");
+      Serial.print(idx);
+      Serial.print(" --> ");
+      
+      for(int i=0; i<10; i++){
+       Serial.print(test_buffer[i]);
+      }
+      Serial.println();
+      //set index awal += 584
+      //copy data dari index x sampai dindex n
+      delay(3000);
     }
-        
-//    if(length_data > 10){
-//        int *secondHalf = malloc(sisa* sizeof(int));
-//        if (!secondHalf) {
-//          /* handle error */
-//          Serial.println(F("FAIL: Malloc fail!"));
-//        }
 //
-//        memcpy(secondHalf, array + 10, sisa * sizeof(int));
-//
-//         
-//        for(int i=0; i<sisa; i++){
-//           Serial.println(secondHalf[i]);
-//        }
+//    memset(test_buffer,0,100);
+//    //array tujuan, idx source, idx destination / panjang data
+//    memcpy(test_buffer, array+4, 5 * sizeof(int));
+//    for(int i=0; i<10; i++){
+//       Serial.print(test_buffer[i]);
 //    }
-   
-    //Serial.println(length_data);
-    //Serial.println(sizeof(firstHalf));
-   
+    
+//    length_data = sizeof(array)/sizeof(int);
+//    int *firstHalf = malloc(10 * sizeof(int));
+//    if (!firstHalf) {
+//      /* handle error */
+//      Serial.println(F("FAIL: Malloc fail!"));
+//    }
+////    memcpy( destination + N, source + N, sourceLen - N );
+////    memcpy(firstHalf, array, 10 * sizeof(int));
+//    memcpy(firstHalf, array + 2, 10 * sizeof(int));
+//    sisa = length_data - 10;
+
     
     for ( ;; )
     {
