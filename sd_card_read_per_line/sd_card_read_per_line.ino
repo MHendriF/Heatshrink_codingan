e4e97b8ac295f5887284239910dcd3a37242d0c9
M Hendri F <hendrifebriansyah28@gmail.com>
2018-05-26 11:58:40 +0700

    pendukung

---
+++ sd_card_read_per_line/sd_card_read_per_line.ino
@@ -0,0 +1,49 @@
+/*
+ *  Arduino SD Card Tutorial Example
+ *  
+ *  by Dejan Nedelkovski, www.HowToMechatronics.com
+ */
+#include <SD.h>
+#include <SPI.h>
+File myFile;
+String buffer;
+int pinCS = 53; // Pin 10 on Arduino Uno
+
+void setup() {
+    
+  Serial.begin(9600);
+  pinMode(pinCS, OUTPUT);
+  
+  // SD Card Initialization
+  if (SD.begin())
+  {
+    Serial.println("SD card is ready to use.");
+  } else
+  {
+    Serial.println("SD card initialization failed");
+    return;
+  }
+  
+  // Create/Open file 
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  // Reading the file
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("Read:");
+    // Reading the whole file
+    while (myFile.available()) {
+      buffer = myFile.readStringUntil('\n');
+      Serial.println(buffer); //Printing for debugging purpose
+      delay(4000);
+   }
+    myFile.close();
+  }
+  else {
+    Serial.println("error opening test.txt");
+  }
+  
+}
+void loop() {
+  // empty
+}
